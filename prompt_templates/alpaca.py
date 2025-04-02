c2e85c6a9f989c9e1a25cdcecd5b458da43c66a2
Aleix Sant <aleix.santsavall@telefonica.com>
2025-04-02 13:56:53 +0000

    Upload prompt used for fine-tuning with alpaca

---
+++ prompt_templates/alpaca.py
@@ -0,0 +1,9 @@
+PROMPT = {
+    "prompt_input": (
+        "Summarize the following text into one concise sentence that captures its main idea.\n\n"
+        "### Text:\n{instruction}\n\n### Input:"
+        "\n{input}\n\n### Summary:\n"),
+    "prompt_no_input": (
+        "Summarize the following text into one concise sentence that captures its main idea.\n\n"
+        "### Text:\n{instruction}\n\n### Summary:\n"),
+}
