a327d28208f8c132fba385e34a9e99e6e7582c80
Aleix Sant <aleix.santsavall@telefonica.com>
2025-10-01 16:07:02 +0200

    Add prompt templates

---
+++ federatedscope/prompt_templates/alpaca_cleaned_8c.py
@@ -0,0 +1,98 @@
+PROMPT = {
+    "en": {
+        "prompt_input": (
+            "Below is an instruction that describes a task, "
+            "paired with an input that provides further context. "
+            "Write a response that appropriately completes the request.\n\n"
+            "### Instruction:\n{instruction}\n\n### Input:"
+            "\n{input}\n\n### Response:"),
+        "prompt_no_input": (
+            "Below is an instruction that describes a task. "
+            "Write a response that appropriately completes the request.\n\n"
+            "### Instruction:\n{instruction}\n\n### Response:"),
+    },
+    "ca": {
+        "prompt_input": (
+            "A continuació es mostra una instrucció que descriu una tasca, "
+            "juntament amb una entrada que proporciona més context. "
+            "Escriviu una resposta que completi adequadament la sol·licitud.\n\n"
+            "### Instrucció:\n{instruction}\n\n### Entrada:"
+            "\n{input}\n\n### Resposta:"),
+        "prompt_no_input": (
+            "A continuació es mostra una instrucció que descriu una tasca. "
+            "Escriviu una resposta que completi adequadament la sol·licitud.\n\n"
+            "### Instrucció:\n{instruction}\n\n### Resposta:"),
+    },
+    "es": {
+        "prompt_input": (
+            "A continuación, se muestra una instrucción que describe una tarea, "
+            "acompañada de una entrada que proporciona más contexto. "
+            "Escriba una respuesta que complete la solicitud adecuadamente.\n\n"
+            "### Instrucción:\n{instruction}\n\n### Entrada:"
+            "\n{input}\n\n### Respuesta:"),
+        "prompt_no_input": (
+            "A continuación, se muestra una instrucción que describe una tarea. "
+            "Escriba una respuesta que complete la solicitud adecuadamente.\n\n"
+            "### Instrucción:\n{instruction}\n\n### Respuesta:"),
+    },
+    "de": {
+        "prompt_input": (
+            "Nachfolgend finden Sie eine Anweisung, die eine Aufgabe beschreibt, "
+            "gepaart mit einer Eingabe, die weiteren Kontext liefert. "
+            "Schreiben Sie eine Antwort, die die Anfrage angemessen ergänzt.\n\n"
+            "### Anweisung:\n{instruction}\n\n### Eingabe:"
+            "\n{input}\n\n### Antwort:"),
+        "prompt_no_input": (
+            "Nachfolgend finden Sie eine Anweisung, die eine Aufgabe beschreibt. "
+            "Schreiben Sie eine Antwort, die die Anfrage angemessen ergänzt.\n\n"
+            "### Anweisung:\n{instruction}\n\n### Antwort:"),
+    },
+    "da": {
+        "prompt_input": (
+            "Nedenfor er en instruktion, der beskriver en opgave, "
+            "parret med et input, der giver yderligere kontekst. "
+            "Skriv et svar, der fuldfører anmodningen på passende vis.\n\n"
+            "### Instruktion:\n{instruction}\n\n### Input:"
+            "\n{input}\n\n### Svar:"),
+        "prompt_no_input": (
+            "Nedenfor er en instruktion, der beskriver en opgave. "
+            "Skriv et svar, der fuldfører anmodningen på passende vis.\n\n"
+            "### Instruktion:\n{instruction}\n\n### Svar:"),
+    },
+    "eu": {
+        "prompt_input": (
+            "Jarraian, zeregin bat deskribatzen duen instrukzio bat dago, "
+            "testuinguru gehiago ematen duen sarrera batekin lotuta. "
+            "Idatzi eskaera behar bezala betetzen duen erantzun bat.\n\n"
+            "### Instrukzioa:\n{instruction}\n\n### Sarrera:"
+            "\n{input}\n\n### Erantzuna:"),
+        "prompt_no_input": (
+            "Jarraian, zeregin bat deskribatzen duen instrukzio bat dago. "
+            "Idatzi eskaera behar bezala betetzen duen erantzun bat.\n\n"
+            "### Instrukzioa:\n{instruction}\n\n### Erantzuna:"),
+    },
+    "hr": {
+        "prompt_input": (
+            "U nastavku je instrukcija koja opisuje zadatak, "
+            "uparena s ulazom koji pruža daljnji kontekst. "
+            "Napišite odgovor koji na odgovarajući način dovršava zahtjev.\n\n"
+            "### Uputa:\n{instruction}\n\n### Ulaz:"
+            "\n{input}\n\n### Odgovor:"),
+        "prompt_no_input": (
+            "U nastavku je instrukcija koja opisuje zadatak. "
+            "Napišite odgovor koji na odgovarajući način dovršava zahtjev.\n\n"
+            "### Uputa:\n{instruction}\n\n### Odgovor:"),
+    },
+    "sr": {
+        "prompt_input": (
+            "Испод је инструкција која описује задатак, "
+            "упарена са уносом који пружа додатни контекст. "
+            "Напишите одговор који на одговарајући начин допуњује захтев.\n\n"
+            "### Инструкција:\n{instruction}\n\n### Унос:"
+            "\n{input}\n\n### Одговор:"),
+        "prompt_no_input": (
+            "Испод је инструкција која описује задатак. "
+            "Напишите одговор који на одговарајући начин допуњује захтев.\n\n"
+            "### Инструкција:\n{instruction}\n\n### Одговор:"),
+    }
+}
